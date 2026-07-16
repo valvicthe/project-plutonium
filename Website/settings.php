e519a0ade88f7323f9d45f098f94af14aed1e469
valvicthe <louie12brett@icloud.com>
2026-07-16 18:56:51 +0100

    edit site

--- Website/settings.php
+++ Website/settings.php
@@ -2,8 +2,8 @@
 //Shit i made that makes the site cleaner and easier to set up - Flarf
 define("SITENAME","Project Plutonium");
 define("CURRENCYNAME","Robux");
-define("SITEDOMAIN","https://projvap.cf");
-define("SMALLLOGOPATH","https://projvap.cf/img/logosmall.png");
-define("FULLLOGOPATH","https://projvap.cf/img/logo.png?v1");
+define("SITEDOMAIN","https://plutonium.up.railway.app/");
+define("SMALLLOGOPATH","https://plutonium.up.railway.app/img/logosmall.png");
+define("FULLLOGOPATH","https://plutonium.up.railway.app/img/logo.png?v1");
 define("YOUTUBETRAILERLINK","https://www.youtube-nocookie.com/embed/Mvf9JCG2KaE");
 ?>
