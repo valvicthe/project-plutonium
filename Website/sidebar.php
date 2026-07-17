3c3ab6a0f97e60d054307a39c8cdd622ab02170a
valvicthe <louie12brett@icloud.com>
2026-07-17 17:23:20 +0100

    edit sidebar

--- Website/sidebar.php
+++ Website/sidebar.php
@@ -14,7 +14,10 @@ $alert_row = $alert_stmt->fetch(PDO::FETCH_ASSOC);
 
 <!-- Sidebar -->
 <div class="sidebar">
-    <a href="<?php echo SITEDOMAIN;?>/dashboard.php"><i class="fas fa-home mr-2"></i> Dashboard</a>
+    <a href="<?php echo SITEDOMAIN;?>/dashboard.php"><i class="fas fa-home mr-2"></i> Home</a>
     <a href="<?php echo SITEDOMAIN;?>/profile"><i class="fas fa-user mr-2"></i> Profile</a>
     <a href="<?php echo SITEDOMAIN;?>/friends"><i class="fas fa-users mr-2"></i> Friends</a>
+    <a href="<?php echo SITEDOMAIN;?>/avatar"><i class="fas fa-users mr-2"></i> Avatar</a>
+    <a href="<?php echo SITEDOMAIN;?>/groups"><i class="fas fa-users mr-2"></i> Groups</a>
+    <a href="<?php echo SITEDOMAIN;?>/trade"><i class="fas fa-users mr-2"></i> Trade</a>
 </div>
